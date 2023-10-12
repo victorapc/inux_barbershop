47d4f37f1c14e2d13077af9f2a0cdd287fa4f4bb
victor <victor.alexandre@atlantasistemas.com>
2023-10-12 10:29:03 -0300

    Redirecionamento SplashScreen

---
+++ lib/src/features/splash/splash_vm.dart
@@ -0,0 +1,40 @@
+import 'package:inux_barbershop/src/core/constants/local_storage_keys.dart';
+import 'package:inux_barbershop/src/core/providers/application_providers.dart';
+import 'package:inux_barbershop/src/model/user_model.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+part 'splash_vm.g.dart';
+
+enum SplashState {
+  initial,
+  login,
+  loggedADM,
+  loggedEmployee,
+  error,
+}
+
+@riverpod
+class SplashVm extends _$SplashVm {
+  @override
+  Future<SplashState> build() async {
+    final sp = await SharedPreferences.getInstance();
+
+    if (sp.containsKey(LocalStorageKeys.accessToken)) {
+      ref.invalidate(getMeProvider);
+      ref.invalidate(getMyBarberShopProvider);
+
+      try {
+        final userModel = await ref.watch(getMeProvider.future);
+        return switch (userModel) {
+          UserModelADM() => SplashState.loggedADM,
+          UserModelEmployee() => SplashState.loggedEmployee,
+        };
+      } catch (e) {
+        return SplashState.login;
+      }
+    }
+
+    return SplashState.login;
+  }
+}
